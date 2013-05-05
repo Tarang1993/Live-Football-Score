8d3009992578a3d972bad36693a935e222dc6809
Tarang <tarangrockr@gmail.com>
2013-05-05 16:05:18 +0530

    added notification bar

--- soccer_score.py
+++ soccer_score.py
@@ -3,23 +3,54 @@ import urllib2
 import StringIO
 import xml.dom.minidom
 from Tkinter import *
+import commands
+import sys
+from subprocess import call
 import tkMessageBox
 from xml.dom import minidom
-url = "http://www.scorespro.com/rss/live-soccer.xml"
-u = urllib2.urlopen(url)
-xml = minidom.parse(u)
-s=''
+from time import sleep
+import threading
+
+time = 300
+
 def showScore():
+    url = "http://www.scorespro.com/rss/live-soccer.xml"
+    u = urllib2.urlopen(url)
+    xml = minidom.parse(u)
     s=''
     for element in xml.getElementsByTagName('title'):
         s=s+element.firstChild.nodeValue
         s=s+'\n'
     return s
-root = Tk()
-root.title("Live Soccer Score!")
-score = showScore()
-var = StringVar()
-soccer_score = Message(root , textvariable = var , relief = RAISED)
-var.set(score)
-soccer_score.pack()
-root.mainloop()
+
+
+def main():
+    str="notify-send \"" + showScore() + "\""
+    commands.getstatusoutput(str)
+
+class Window(threading.Thread):
+	def __init__(self):
+         
+                threading.Thread.__init__(self)
+                self.start()
+        def callback(self):
+                self.root.quit()
+
+        def run(self):
+                self.root = Tk()
+                self.root.title("Live Soccer Score!")
+                score = showScore()
+                self.var = StringVar()
+                soccer_score = Message(self.root , textvariable = self.var , relief = RAISED)
+                self.var.set(score)
+                soccer_score.pack()
+
+		self.root.mainloop()
+
+window = Window()
+	
+while True:
+    if __name__ == "__main__":
+        main()
+        sleep(time)
+    
